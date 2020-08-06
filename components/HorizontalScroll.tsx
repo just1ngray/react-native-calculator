3c4c6a179c2e0bddee59982ef64d1422e29cffb9
just1ngray <just1ngray@outlook.com>
2020-08-06 00:38:00 -0300

    Added a right margin

--- components/HorizontalScroll.tsx
+++ components/HorizontalScroll.tsx
@@ -18,6 +18,7 @@ export default function HorizontalScroll(props: Props) {
     return (
         <View style={{ 
             height: props.maxFontSize,
+            marginLeft: 20
         }}>
             <ScrollView
                     horizontal={true} 
